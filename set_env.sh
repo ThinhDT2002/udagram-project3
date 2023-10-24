f2bbb38e4a7022c70931d6bb1fb64e57337e5107
ThinhDoTien <dotienthinh21102002@gmail.com>
2023-10-24 18:51:27 +0700

    Initial Project

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=admin123
+export POSTGRES_HOST=udagram-project3.ckc4jbbbzuva.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::my-365865334136-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+export HOME=C:\Users\DoTienThinh
